e7353ff9cdc7c78cfe8b06515baa10f4b2b90233
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-22 17:06:34 +0200

    Moved the controller to the main file

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const ctrlMain = require("../controllers/main");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.index);
 
 module.exports = router;
