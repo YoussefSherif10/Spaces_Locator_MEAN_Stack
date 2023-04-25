f2a076e130450954ebbc62c3db17fd2d6798454d
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-25 18:20:52 +0200

    Moved the data from the views to the controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,71 @@
 // the home page that displays the main list of locations
 const homeList = (req, res) => {
-    res.render('locations-list', {});
+    res.render('locations-list', {
+        pageHeaders: {
+            title: 'Locator',
+            strapLine: 'Find places to work with wifi near you!'
+        },
+        locations:[
+            {
+            name: "Star cups",
+            address: "125 High Street, Reading, RG6 1PS",
+            rating: 3,
+            facilities: ["Hot Drinks", "Food", "Wifi"],
+            distance: "100m"
+            },
+            {
+                name: "Cafe Hero",
+                address: "125 High Street, Reading, RG6 1PS",
+                rating: 4,
+                facilities: ["Hot Drinks", "Food", "Wifi", "Comfort"],
+                distance: "200m"
+            },
+            {
+                name: "Burger Queen",
+                address: "125 High Street, Reading, RG6 1PS",
+                rating: 2,
+                facilities: ["Food", "Wifi"],
+                distance: "250m"
+            }
+        ]
+    });
 }
 
 // details of a location
 const locationInfo = (req, res) => {
-    res.render('location-info', {});
+    res.render('location-info', {
+        title: "Star cups",
+        rating: 3,
+        address: "125 High Street, Reading, RG6 1PS",
+        openingHours: ["Monday - Friday : 7:00am - 7:00pm", "Saturday : 8:00am - 5:00pm", "Sunday : closed"],
+        facilities: ["Hot Drinks", "Food", "Wifi"],
+        imgSrc: "http://maps.googleapis.com/maps/api/staticmap?center=51.455041,-0.9690884&zoom=17&size=400x350&sensor=false&markers=51.455041,-0.9690884&scale=2&key=AIzaSyDX0W0bSoP2P81jHe4JcC1JaGXMRC4kXCo",
+        reviews: [
+            {
+                rating: 4,
+                name: "Youssef Sherif",
+                date: "25 April 2023",
+                review: "what a great place"
+            },
+            {
+                rating: 3,
+                name: "Nahed Ahmed",
+                date: "25 April 2023",
+                review: "It was okay. Coffee wasn't great."
+            },
+            {
+                rating: 1,
+                name: "Ibrahim Fekry",
+                date: "25 April 2023",
+                review: "Didn't like it."
+            }
+        ]
+    });
 }
 
 // adding new review
 const addReview = (req, res) => {
-    res.render('location-review-form', {title: 'review'});
+    res.render('location-review-form', {title: 'Star cups'});
 }
 
 module.exports = {homeList, locationInfo, addReview};
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,18 @@
 const about = (req, res) => {
-    res.render('about', {title: 'About'})
+    res.render('about', {
+        title: 'About',
+        paragraphs: [
+            {text: `Spaces lists nearby places with Wi-Fi where people can go to get some work done. 
+            It also displays facilities, opening times, a rating, and a location map for each place. 
+            Users will be able to log in and submit ratings and reviews.`},
+            {text: `This application has some grounding in the real world. 
+            Location-based applications themselves aren’t particularly new and come in a few guises. 
+            Swarm and Facebook Check In list everything nearby that they can and crowdsource data for new places 
+            and information updates. Urbanspoon helps people find nearby places to eat, allowing a user to search 
+            on price bracket and type of cuisine. Even companies like Starbucks and McDonald’s have sections of 
+            their applications that help users find the nearest one.`}
+        ]
+    })
 }
 
 module.exports = {about};
