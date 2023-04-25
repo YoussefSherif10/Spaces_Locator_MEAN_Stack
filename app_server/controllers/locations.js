fa5151c09bce3d98affd926ea65f25c9fa242e21
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-25 02:53:54 +0200

    Added the home and the details views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 // the home page that displays the main list of locations
 const homeList = (req, res) => {
-    res.render('index', {title: 'Home'});
+    res.render('locations-list', {});
 }
 
 // details of a location
 const locationInfo = (req, res) => {
-    res.render('index', {title: 'details'});
+    res.render('location-info', {});
 }
 
 // adding new review
@@ -14,3 +14,5 @@ const addReview = (req, res) => {
 }
 
 module.exports = {homeList, locationInfo, addReview};
+
+// AIzaSyDX0W0bSoP2P81jHe4JcC1JaGXMRC4kXCo  google key
