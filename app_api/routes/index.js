6e7a8ea33ad7df3a693a48a31e1c11d28d96d9da
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-26 20:24:14 +0200

    Created the routes of the API

---
+++ app_api/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const ctrlLocations = require('../controllers/locations');
+const ctrlReviews = require('../controllers/reviews');
+
+/* Locations */
+router.route('/locations').get(ctrlLocations.locationsList).post(ctrlLocations.createLocation);
+router.route('/locations/:locationId').get(ctrlLocations.locationsReadOne).put(ctrlLocations.locationsUpdateOne)
+    .delete(ctrlLocations.locationsDeleteOne);
+
+/* Reviews */
+router.route('/locations/:locationId/reviews').post(ctrlReviews.createReview);
+router.route('/locations/:locationId/reviews/:reviewName')
+    .get(ctrlReviews.reviewsReadOne)
+    .put(ctrlReviews.reviewsUpdateOne)
+    .delete(ctrlReviews.reviewsDeleteOne);
+
+module.exports = router;
