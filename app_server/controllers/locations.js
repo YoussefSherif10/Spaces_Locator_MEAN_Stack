e35ef0377bac95ef6e1c298a28e219218b35703b
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-28 07:09:32 +0300

    Moved the data from locations controller to a request from the API

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,33 +1,87 @@
-// the home page that displays the main list of locations
-const homeList = (req, res) => {
+// request package allows to make http calls to the api
+const request = require('request')
+
+/**
+ * set the baseUrl according to NODE_ENV as the request package needs a full qualified address
+ * that differs from dev to production
+ */
+const apiOptions = {
+    server: 'http://localhost:3000/api'
+};
+if (process.env.NODE_ENV === 'production')
+    apiOptions.server = 'https://spaces-locator.onrender.com/api'; // the web address
+
+/**
+ * this function is used to render the data and the views to send back a response
+ * it also deals with an error response and empty response scenarios
+ * @param req : requestObject
+ * @param res : response
+ * @param body : listOfObjects
+ */
+const renderHomePage = (req, res, body) => {
+    let message = null;
+
+    // case of error string
+    if (!(body instanceof Array)) {
+        message = "API lookup error";
+        body = [];  // prevent the view from throwing an error
+    }
+    // case of no available places
+    else if (!body.length)
+        message = "No nearby places";
+
     res.render('locations-list', {
         pageHeaders: {
             title: 'Locator',
             strapLine: 'Find places to work with wifi near you!'
         },
-        locations:[
-            {
-            name: "Star cups",
-            address: "125 High Street, Reading, RG6 1PS",
-            rating: 3,
-            facilities: ["Hot Drinks", "Food", "Wifi"],
-            distance: "100m"
-            },
-            {
-                name: "Cafe Hero",
-                address: "125 High Street, Reading, RG6 1PS",
-                rating: 4,
-                facilities: ["Hot Drinks", "Food", "Wifi", "Comfort"],
-                distance: "200m"
-            },
-            {
-                name: "Burger Queen",
-                address: "125 High Street, Reading, RG6 1PS",
-                rating: 2,
-                facilities: ["Food", "Wifi"],
-                distance: "250m"
-            }
-        ]
+        locations: body,
+        message: message
+    });
+}
+
+/**
+ * put the distance in a readable format to be shown to the user
+ * @param distance : distance
+ * @returns {string} : formatedDistance
+ */
+const formatDistance = (distance) => {
+    let thisDistance = 0, unit = 'm';
+    if (distance > 1000) {
+        thisDistance = parseFloat(distance / 1000).toFixed(1);
+        unit = 'km';
+    }
+    else
+        thisDistance = Math.floor(distance);
+    return thisDistance + unit;
+}
+
+// the home page that displays the main list of locations
+const homeList = (req, res) => {
+    // defining the parameters of the request
+    const requestOptions = {
+        url: `${apiOptions.server}/locations`,
+        method: 'GET',
+        json: {},
+        qs: {
+            lng:  -0.7992599,
+            lat: 51.378091
+        }
+    }
+
+    // making the api call
+    request(requestOptions, (err, {statusCode}, body) => {
+        // format the distance to be readable
+        let data = [];
+        if (statusCode === 200 && body.length) {
+            data = body.map(loc => {
+                loc.distance = formatDistance(loc.distance);
+                return loc;
+            })
+        }
+
+        // render the processed response
+        renderHomePage(req, res, data);
     });
 }
 
