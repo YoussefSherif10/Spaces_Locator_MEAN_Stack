027b7242f1c1698b7c3d0465f983bb6ab6392a38
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-30 20:20:18 +0300

    Updated the home list component to include a location

--- app-public/src/app/home-list/home-list.component.ts
+++ app-public/src/app/home-list/home-list.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 
+export class Location {
+  constructor(
+      public _id:string,
+      public name:string,
+      public distance:number,
+      public address:string,
+      public rating:number,
+      public facilities:string[],
+  ) {
+  }
+}
+
 @Component({
   selector: 'app-home-list',
-  template: `
-    <p>
-      home-list works!
-    </p>
-  `,
+  templateUrl: '/home-list.component.html',
   styles: [
   ]
 })
 export class HomeListComponent {
-
+  location: Location = {
+    _id: '6448675cf647a97027280250',
+    name: 'Star Cups',
+    distance: 14573.459332764041,
+    address: '125 High Street, Reading, RG61PS',
+    rating: 3,
+    facilities: ['Hot Drinks', 'Food', 'Wifi']
+  }
 }
