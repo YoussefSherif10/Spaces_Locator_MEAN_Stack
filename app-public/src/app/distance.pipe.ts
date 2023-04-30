55c34a1dd221bf257e13f2e0ef7891c32b9d8dc7
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-30 21:29:38 +0300

    Implemented custom distance pipe

---
+++ app-public/src/app/distance.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'distance'
+})
+export class DistancePipe implements PipeTransform {
+
+  transform(distance: number): string {
+    let thisDistance = '0', unit = 'm';
+    if (distance > 1000) {
+      thisDistance = parseFloat(String(distance / 1000)).toFixed(1);
+      unit = 'km';
+    }
+    else
+      thisDistance = Math.floor(distance).toString();
+    return thisDistance + unit;
+  }
+
+}
