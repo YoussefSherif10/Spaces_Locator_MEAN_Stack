2c203b9f879c9fe95a6f1aea400e8f7f86e3d37f
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-30 19:19:22 +0300

    Created home list component and made it the entry one

---
+++ app-public/src/app/home-list/home-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-list',
+  template: `
+    <p>
+      home-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HomeListComponent {
+
+}
