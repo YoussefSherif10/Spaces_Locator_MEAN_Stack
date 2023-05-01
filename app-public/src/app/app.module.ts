8fc64cdbc2bfbad8f8b95e88b6643f00a7cf3d8d
yousseffekry <mr-youssefsherif@hotmail.com>
2023-05-01 13:45:48 +0300

    created the framework component as the main and added routes

--- app-public/src/app/app.module.ts
+++ app-public/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
 import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import { FrameworkComponent } from './framework/framework.component';
+
+const routes = [
+    {path: '', component: HomeListComponent}
+]
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent
   ],
   imports: [
     BrowserModule,
-      HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
